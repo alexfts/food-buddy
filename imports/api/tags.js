dc8e7e0c5e0c2b7fbb693e976e7d5396e377280c
Jennifer <jen.yiu.90@gmail.com>
2019-02-07 10:19:04 -0800

    edit onboard page

--- imports/api/tags.js
+++ imports/api/tags.js
@@ -1,2 +1,14 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+
+// Meteor.methods({
+//     'tags.onboardInput'(tag){
+//         if(){
+//             throw new Meteor.Error(
+//                 'tags.onboardInput.not-authorized',
+//             )
+//         }
+//     }
+// })
+
 export const Tags = new Mongo.Collection('tags');

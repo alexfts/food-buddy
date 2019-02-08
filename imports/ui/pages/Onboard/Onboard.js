305e464ce9bdad7509a414786b778ab0d9975cd8
Jennifer <jen.yiu.90@gmail.com>
2019-02-07 16:36:42 -0800

    add components for Onboard page, reorganize code

---
+++ imports/ui/pages/Onboard/Onboard.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { tags } from '../../../api/tags';
+import { Meteor } from 'meteor/meteor';
+import TagInput from '../../components/TagInput/TagInput';
+import {
+  Button,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography
+} from '@material-ui/core';
+import styles from './styles';
+import { withStyles } from '@material-ui/core/styles';
+
+function getSteps() {
+  return ['Cuisine', 'Food Types', 'Dietary Preferences', 'Extra'];
+}
+
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return 'Select your favourite cuisines:';
+    case 1:
+      return 'Select your favourite Food Types';
+    case 2:
+      return 'Select any dietary preferences';
+    case 3:
+      return 'Extra';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
+
+class Onboard extends React.Component {
+  state = {
+    activeStep: 0
+  };
+
+  handleNext = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep + 1
+    }));
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map(label => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {this.state.activeStep === steps.length ? (
+            <div>
+              <Typography className={classes.instructions}>
+                All steps completed
+              </Typography>
+              <Button onClick={this.handleReset}>Reset</Button>
+            </div>
+          ) : (
+            <div>
+              <Typography className={classes.instructions}>
+                {getStepContent(activeStep)}
+              </Typography>
+              <div>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={this.handleBack}
+                  className={classes.backButton}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleNext}
+                >
+                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+Onboard.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Onboard);

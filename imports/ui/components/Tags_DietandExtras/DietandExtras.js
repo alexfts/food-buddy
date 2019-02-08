a08c79082b26c2d94bb564e4518e31c8fb9ac159
Jennifer <jen.yiu.90@gmail.com>
2019-02-08 09:54:11 -0800

    add const to dietandextras

--- imports/ui/components/Tags_DietandExtras/DietandExtras.js
+++ imports/ui/components/Tags_DietandExtras/DietandExtras.js
@@ -2,4 +2,8 @@ import styles from './styles';
 import { withStyles } from '@material-ui/core/styles';
 import { Button, Typography } from '@material-ui/core';
 
+const DietandExtras = () => {
+  return <Typography>DietandExtras</Typography>;
+};
+
 export default withStyles(styles)(DietandExtras);

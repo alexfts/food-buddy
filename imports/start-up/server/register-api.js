33314921d092b51451b5dffb618064e851de96b2
alexfts <alxfetisova@gmail.com>
2019-02-08 09:31:53 -0800

    Implemented Meteor pub/sub

--- imports/start-up/server/register-api.js
+++ imports/start-up/server/register-api.js
@@ -1,1 +1,2 @@
 import '../../api/tags';
+import '../../api/users';
--- imports/ui/containers/App/index.js
+++ imports/ui/containers/App/index.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
 import './styles.css';
-import AccountsUIWrapper from "../../components/AccountsWrapper";
-import { withTracker } from "meteor/react-meteor-data";
-import { Tags } from "../../../api/tags";
+import AccountsUIWrapper from '../../components/AccountsWrapper';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tags } from '../../../api/tags';
+import { TagCategories } from '../../../api/tagCategories';
+import { Meteor } from 'meteor/meteor';
 
 class App extends Component {
-
-render() {
-
+  render() {
+    console.log(this.props.tags);
+    console.log(this.props.users);
+    console.log(this.props.tagCategories);
     return (
       <div className="app-wrapper">
         <div className="login-wrapper">
           <AccountsUIWrapper />
         </div>
         <p>Welcome to Food Buddy</p>
-    </div>
-    );   
-}
+        <div />
+      </div>
+    );
+  }
 }
 
 export default withTracker(() => {
-    Meteor.subscribe('tags'); 
+  Meteor.subscribe('tags');
+  Meteor.subscribe('tagCategories');
+  Meteor.subscribe('users');
 
   return {
-    currentUser: Meteor.user(), 
-    currentUserId: Meteor.userId(), 
-    tags: Tags.find({}).fetch()
+    currentUser: Meteor.user(),
+    currentUserId: Meteor.userId(),
+    tags: Tags.find({}).fetch(),
+    tagCategories: TagCategories.find({}).fetch(),
+    users: Meteor.users.find({}).fetch()
   };
-
 })(App);

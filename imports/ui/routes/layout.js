f4cf172ed5258bf598b7ad8a36decf6813eeb81e
Vikkkkk <vicguo.99@gmail.com>
2019-02-08 11:14:24 -0800

    stable routing

--- imports/ui/routes/layout.js
+++ imports/ui/routes/layout.js
@@ -6,17 +6,28 @@ import Onboard from '../pages/Onboard';
 import Login from '../pages/Login';
 import FullScreenLoader from '../components/Loader';
 import { withTracker } from 'meteor/react-meteor-data';
+import { TagCategories } from '../../api/tagCategories';
+import { Tags } from '../../api/tags';
+import { Accounts } from 'meteor/accounts-base';
 
 // import NavBar from '../components/NavBar/NavBar';
 
-const Layout = ({ currentUserId }) => {
-  if (!currentUserId) return <FullScreenLoader inverted />;
-  if (currentUserId) {
+const Layout = ({ loggedOut }) => {
+  //   if (loading) {
+  //     // console.log(users.length);
+  //     return <FullScreenLoader inverted />;
+  //   }
+
+  if (!loggedOut) {
+    if (!Meteor.userId()) {
+      return <FullScreenLoader inverted />;
+    }
     return (
       <Switch>
         <Route exact path="/home" component={Home} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/onboard" component={Onboard} />
+        <Redirect from="*" to="/home" />
       </Switch>
     );
   } else {
@@ -30,12 +41,12 @@ const Layout = ({ currentUserId }) => {
 };
 
 export default withTracker(() => {
-  Meteor.subscribe('todos');
   return {
     //the withtracker method lets us write a mongodb query and store the info in a prop called todos.
     //the Todos.find({}) is the mongodb query equivalent for SELECT * FROM Todos.
-    currentUser: Meteor.user()
-    // currentUserId: Meteor.userId()
-    // tags: Tags.find({}).fetch()
+    loggedOut:
+      !Meteor.user() &&
+      !Meteor.loggingIn() &&
+      Accounts.loginServicesConfigured()
   };
 })(Layout);

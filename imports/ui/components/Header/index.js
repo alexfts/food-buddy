11930a3eb5d37aa95c686f23d1bfb907422fc964
Sophie Virtue <sophie.virtue@gmail.com>
2019-02-08 09:45:14 -0800

    Start Header

--- imports/ui/components/Header/index.js
+++ imports/ui/components/Header/index.js
@@ -1,84 +1,110 @@
-// import React, { Component } from "react";
-// import { withStyles } from '@material-ui/core/styles';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import IconButton from '@material-ui/core/IconButton';
-// import Button from '@material-ui/core/Button';
-// import ProfileIcon from '@material-ui/icons/Fingerprint';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Menu from '@material-ui/core/Menu';
-// import styles from './styles';
-// import logo from '../../images/cheers_logo.png';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-// class Header extends Component {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-//     state = {
-//         auth: true,
-//         anchorEl: null,
-//       };
-    
-//       handleMenu = event => {
-//         this.setState({ anchorEl: event.currentTarget });
-//       };
-    
-//       handleClose = () => {
-//         this.setState({ anchorEl: null });
-//       };
-    
-//       render() {
-//         const { classes } = this.props;
-//         const { anchorEl } = this.state;
-//         const open = Boolean(anchorEl);
+class NavBar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
 
-//   return (
-//   <div className={classes.root}>
-//     <AppBar position="static">
-//       <Toolbar>
-//       <Button color="inherit" href="/items"><img src={logo} width="40" alt="Logo" /></Button>
-//         <div className={classes.grow} />
-//           <div>
-//             <IconButton
-//               aria-owns={open ? 'menu-appbar' : undefined}
-//               aria-haspopup="true"
-//               onClick={this.handleMenu}
-//               color="inherit"
-//             >
-//               <ProfileIcon />
-//             </IconButton>
-//             <Menu
-//               id="menu-appbar"
-//               anchorEl={anchorEl}
-//               anchorOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'right',
-//               }}
-//               transformOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'right',
-//               }}
-//               open={open}
-//               onClose={this.handleClose}
-//             >
-//               <MenuItem onClick={this.handleClose}
-//               component={Link}
-//               to={`/profile/${this.props.user.id}`}
-//               >
-//               Your Profile</MenuItem>
-//               <MenuItem onClick={e => {
-                
-//               }}
-//               >
-//               Sign Out</MenuItem> 
-//             </Menu>
-//           </div>
-//       </Toolbar>
-//     </AppBar>
-//   </div>);
-//   }
-// }
+  handleChange = event => {
+    this.setState({ auth: event.target.checked });
+  };
 
- 
-// export default compose(withStyles(styles), withRouter)(Header);
-  
-// // export default Header;
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />
+            }
+            label={auth ? 'Logout' : 'Login'}
+          />
+        </FormGroup>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Photos
+            </Typography>
+            {auth && (
+              <div>
+                <IconButton
+                  aria-owns={open ? 'menu-appbar' : undefined}
+                  aria-haspopup="true"
+                  onClick={this.handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+NavBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(NavBar);

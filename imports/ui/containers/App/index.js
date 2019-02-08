00e019b3bbe5e3651cb47441e97cfbcd799ed359
Vikkkkk <vicguo.99@gmail.com>
2019-02-08 14:58:29 -0800

    adding login form and sign up event

--- imports/ui/containers/App/index.js
+++ imports/ui/containers/App/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
-import AccountsWrapper from '../../components/AccountsWrapper';
+import LoginForm from '../../components/LoginForm/LoginForm';
 import Header from '../../components/Header';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Tags } from '../../../api/tags';
@@ -14,14 +14,10 @@ class App extends Component {
   render() {
     return (
       <div className="app-wrapper">
-        <div className="login-wrapper">
-          <AccountsWrapper />
-        </div>
+        <div className="login-wrapper">{/* <LoginForm /> */}</div>
         <BrowserRouter>
           <Layout />
         </BrowserRouter>
-
-        <Typography>Welcome to Food Buddy</Typography>
       </div>
     );
   }

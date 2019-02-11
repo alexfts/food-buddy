8e877874fa66b21cbb925aef2ebafae256494b0e
Vikkkkk <vicguo.99@gmail.com>
2019-02-11 15:11:23 -0800

    adding bubbles component

---
+++ imports/ui/components/Bubbles/Bubbles.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { TagCategories } from '../../../api/tagCategories';
+import { Tags } from '../../../api/tags';
+import Chip from '@material-ui/core/Chip';
+import styles from './styles';
+
+class Bubbles extends React.Component {
+  state = {
+    selectedTags: []
+  };
+
+  handleSelect = tag => {
+    this.state.selectedTags.some(t => t === tag._id)
+      ? this.setState(
+          {
+            selectedTags: this.state.selectedTags.filter(t => {
+              return t !== tag._id;
+            })
+          },
+          () => {
+            Meteor.call('users.updateUserTags', this.state.selectedTags);
+          }
+        )
+      : this.setState(
+          { selectedTags: [...this.state.selectedTags, tag._id] },
+          () => {
+            Meteor.call('users.updateUserTags', this.state.selectedTags);
+          }
+        );
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.tags.map(tag => (
+          <Chip label={tag.title} onClick={() => this.handleSelect(tag)} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default withTracker(() => {
+  Meteor.subscribe('tags');
+  Meteor.subscribe('tagCategories');
+  return {
+    tags: Tags.find({}).fetch(),
+    tagCategories: TagCategories.find({}).fetch()
+  };
+})(withStyles(styles, { withTheme: true })(Bubbles));

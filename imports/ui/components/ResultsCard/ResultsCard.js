bc830797a4c4bce745969bb5ddf27e262256d696
alexfts <alxfetisova@gmail.com>
2019-02-12 13:31:52 -0800

    Refactor components to pull from google maps

--- imports/ui/components/ResultsCard/ResultsCard.js
+++ imports/ui/components/ResultsCard/ResultsCard.js
@@ -9,43 +9,34 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 function MediaCard(props) {
-  const { classes } = props;
- 
+  const { classes, places } = props;
+  console.log('CARD PLACES', places);
   return (
     <Card className={classes.card}>
-   
       <CardActionArea>
-{/* Link to website from api details */}
+        {/* Link to website from api details */}
         <a
-            href="https://google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={classes.hrefLink}
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.hrefLink}
         >
-        {/* image from photo details */}
-        <CardMedia
-          className={classes.media}
-          component='img'
-          src={'https://picsum.photos/200'}
-          title="Restaurant Image"
-        />
-        <CardContent>
-          <Typography component="h2">
-            Pull restaurant name
-          </Typography>
-          <Typography component="p">
-            Pull restaurant rating
-          </Typography>
-          <Typography component="p">
-            Pull restaurant price_level
-          </Typography>
-          <Typography component="p">
-            Pull restaurant distance
-          </Typography>
-          <Typography component="p">
-            Pull restaurant formatted_address from details
-          </Typography>
-        </CardContent>
+          {/* image from photo details */}
+          <CardMedia
+            className={classes.media}
+            component="img"
+            src={'https://picsum.photos/200'}
+            title="Restaurant Image"
+          />
+          <CardContent>
+            <Typography component="h2">Pull restaurant name</Typography>
+            <Typography component="p">Pull restaurant rating</Typography>
+            <Typography component="p">Pull restaurant price_level</Typography>
+            <Typography component="p">Pull restaurant distance</Typography>
+            <Typography component="p">
+              Pull restaurant formatted_address from details
+            </Typography>
+          </CardContent>
         </a>
       </CardActionArea>
     </Card>
@@ -53,14 +44,10 @@ function MediaCard(props) {
 }
 
 MediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(MediaCard);
-
-
-
-
 
 // React Native version with edits to convert to react
 
@@ -68,7 +55,7 @@ export default withStyles(styles)(MediaCard);
 // import {
 //   Text,
 //   View,
-//   List, 
+//   List,
 //   ListItem,
 //   FlatList,
 //   ActivityIndicator
@@ -179,9 +166,9 @@ export default withStyles(styles)(MediaCard);
 //     this.fetchData();
 //   };
 //   render() {
-  
+
 //     return (
-//       <View>    
+//       <View>
 //       <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }} >
 //       <FlatList
 //         data={this.state.data}
@@ -189,7 +176,7 @@ export default withStyles(styles)(MediaCard);
 //         ListHeaderComponent={this.renderHeader}
 //         ListFooterComponent={this.renderFooter}
 //         renderItem={({ item }) =>{
-   
+
 //           const rating = item.rating ? item.rating : 'na'
 
 //           return (<View><ListItem
@@ -282,7 +269,7 @@ export default withStyles(styles)(MediaCard);
 //       });
 //     })
 //   };
-//  
+//
 //   handleRefresh = () => {
 //     this.setState(
 //       {
@@ -299,9 +286,9 @@ export default withStyles(styles)(MediaCard);
 //     this.fetchData();
 //   };
 //   render() {
-  
+
 //     return (
-//       <View>    
+//       <View>
 //       <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }} >
 //       <FlatList
 //         data={this.state.data}
@@ -309,7 +296,7 @@ export default withStyles(styles)(MediaCard);
 //         ListHeaderComponent={this.renderHeader}
 //         ListFooterComponent={this.renderFooter}
 //         renderItem={({ item }) =>{
-   
+
 //           const rating = item.rating ? item.rating : 'na'
 
 //           return (<View><ListItem

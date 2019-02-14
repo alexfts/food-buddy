68e05353fffc5925b4fb0f9f4972e63d8ffced00
Jennifer <jen.yiu.90@gmail.com>
2019-02-14 09:40:56 -0800

    update resultscard

--- imports/ui/components/ResultsCard/ResultsCard.js
+++ imports/ui/components/ResultsCard/ResultsCard.js
@@ -2,70 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-<<<<<<< HEAD
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import googleMapsWebsite from '../../../api/googleMaps';
-
-function MediaCard(props) {
-  const { classes, places } = props;
-
-  return places.map(place => {
-    const placeid = place.place_id;
-    Meteor.call('googleMapsWebsite.geocode', placeid);
-
-    // // let url = `https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyCsLQmoYlsOqd5yWQpnkbwbpa76UmYwz8E&placeid=${place.place_id}`
-    // // fetch(url)
-    // //   .then(function(response) {
-    // //   return response.json();
-    // //   })
-    // //   .then(function(myJson) {
-    // //   console.log(JSON.stringify(myJson.result.website));
-    // //   });
-    return (
-      <Card className={classes.card}>
-        <CardActionArea>
-          {/* Link to website from api details */}
-          <a
-            href="https://google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={classes.hrefLink}
-          >
-            {/* image from photo details */}
-            <CardMedia
-              className={classes.media}
-              component="img"
-              src={'https://picsum.photos/200'}
-              title="Restaurant Image"
-            />
-            <CardContent>
-              <Typography component="h2">{place.name}</Typography>
-              <Typography component="p">Rating: {place.rating}</Typography>
-              <Typography component="p">
-                Price Level: {place.price_level}
-              </Typography>
-              <Typography component="p">{place.vicinity}</Typography>
-            </CardContent>
-          </a>
-        </CardActionArea>
-      </Card>
-    );
-  });
-  import {
-    Card,
-    CardActionArea,
-    CardContent,
-    CardMedia,
-    Typography,
-    Drawer,
-    List,
-    ListItem
-  } from '@material-ui/core/';
-=======
 import {
   Card,
   CardActionArea,
@@ -76,12 +12,9 @@ import {
   List,
   ListItem
 } from '@material-ui/core/';
->>>>>>> 49f74d3e16c260f75a2b92095adbacebfbcd66f5
-
-  // function MediaCard(props) {
-  //   const { classes, places } = props;
-  //   console.log(places);
-
+function MediaCard(props) {
+  const { classes, places } = props;
+  console.log(places);
   return (
     <div className={classes.root}>
       <Drawer
@@ -94,8 +27,8 @@ import {
       >
         <List>
           {places.map(place => {
-              const placeid = place.place_id;
-              Meteor.call('googleMapsWebsite.geocode', placeid);
+            const placeid = place.place_id;
+            Meteor.call('googleMapsWebsite.geocode', placeid);
             return (
               <ListItem>
                 <Card className={classes.card}>
@@ -137,9 +70,7 @@ import {
     </div>
   );
 }
-
 MediaCard.propTypes = {
   classes: PropTypes.object.isRequired
 };
-
 export default withStyles(styles)(MediaCard);

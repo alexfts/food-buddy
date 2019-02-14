49f74d3e16c260f75a2b92095adbacebfbcd66f5
Sophie Virtue <sophie.virtue@gmail.com>
2019-02-14 09:35:50 -0800

    Clean up unsused code

--- imports/ui/components/ResultsCard/ResultsCard.js
+++ imports/ui/components/ResultsCard/ResultsCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
@@ -78,41 +78,3 @@ MediaCard.propTypes = {
 };
 
 export default withStyles(styles)(MediaCard);
-
-
-// function MediaCard(props) {
-//   const { classes, places } = props;
-
-//   return places.map(place => {
-//     const placeid = place.place_id;
-//     Meteor.call('googleMapsWebsite.geocode', placeid);
-//     return (
-//       <Card className={classes.card}>
-//         <CardActionArea>
-//           {/* Link to website from api details */}
-//           <a
-//             href="https://google.com"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className={classes.hrefLink}
-//           >
-//             {/* image from photo details */}
-//             <CardMedia
-//               className={classes.media}
-//               component="img"
-//               src={'https://picsum.photos/200'}
-//               title="Restaurant Image"
-//             />
-//             <CardContent>
-//               <Typography component="h2">{place.name}</Typography>
-//               <Typography component="p">Rating: {place.rating}</Typography>
-//               <Typography component="p">
-//                 Price Level: {place.price_level}
-//               </Typography>
-//               <Typography component="p">{place.vicinity}</Typography>
-//             </CardContent>
-//           </a>
-//         </CardActionArea>
-//       </Card>
-//     );
-//   });

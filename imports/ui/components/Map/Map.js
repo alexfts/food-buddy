055d55bc35d7527f8760f1784446eca8276fa922
Vikkkkk <vicguo.99@gmail.com>
2019-02-17 20:44:43 -0800

    added map clusters to help with zoom lag

--- imports/ui/components/Map/Map.js
+++ imports/ui/components/Map/Map.js
@@ -11,6 +11,7 @@ import {
   GoogleMap,
   Marker
 } from 'react-google-maps';
+import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer';
 import { MAP } from 'react-google-maps/lib/constants';
 const refs = {
   map: undefined
@@ -408,28 +409,35 @@ class MapComponent extends React.Component {
             defaultCenter={{ lat: 49.2632597, lng: -123.138 }}
             defaultOptions={{ styles: Snazzysnazz }}
           >
-            {props.places &&
-              props.places.map((place, i) => (
-                <Marker
-                  animation={google.maps.Animation.DROP}
-                  onClick={() => this.handleMarkerClicked(i)}
-                  // options={{ icon: { url: ...., scaledSize: ...., size: .... } }}
-                  options={{
-                    icon: {
-                      url:
-                        'https://image.flaticon.com/icons/svg/1149/1149845.svg',
-                      scaledSize: { width: 32, height: 32 }
-                    }
-                  }}
-                  // label={(i + 1).toString()}
-                  label={place.name.split(' ')[0]}
-                  key={i}
-                  position={{
-                    lat: place.geometry.location.lat(),
-                    lng: place.geometry.location.lng()
-                  }}
-                />
-              ))}
+            <MarkerClusterer
+              onClick={props.onMarkerClustererClick}
+              averageCenter
+              enableRetinaIcons
+              gridSize={60}
+            >
+              {props.places &&
+                props.places.map((place, i) => (
+                  <Marker
+                    animation={google.maps.Animation.DROP}
+                    onClick={() => this.handleMarkerClicked(i)}
+                    options={{
+                      icon: {
+                        url:
+                          'https://www.web2market.com/store/pub/media/wysiwyg/icon-map.png',
+                        scaledSize: { width: 40, height: 40 }
+                      },
+                      fontColor: 'white'
+                    }}
+                    // label={(i + 1).toString()}
+                    defaultLabel={place.name.split(' ')[0]}
+                    key={i}
+                    position={{
+                      lat: place.geometry.location.lat(),
+                      lng: place.geometry.location.lng()
+                    }}
+                  />
+                ))}
+            </MarkerClusterer>
           </GoogleMap>
         </Grid>
         <Grid>
